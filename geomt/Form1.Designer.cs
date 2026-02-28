e90d64eff8d807a7f06b97f6dab65018e3131022
Andrey Saberov <saberoff2018@yandex.ru>
2026-02-28 23:35:53 +0300

    всё четко! исправление 1 строкиInitial commit

---
+++ geomt/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace geomt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.x_c = new System.Windows.Forms.TextBox();
+            this.y_c = new System.Windows.Forms.TextBox();
+            this.h1 = new System.Windows.Forms.TextBox();
+            this.w1 = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1120, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "x_c";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1120, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "y_c";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1120, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "h";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1120, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "w";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1120, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "x";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1120, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "y";
+            // 
+            // x_c
+            // 
+            this.x_c.Location = new System.Drawing.Point(1150, 79);
+            this.x_c.Name = "x_c";
+            this.x_c.Size = new System.Drawing.Size(100, 20);
+            this.x_c.TabIndex = 6;
+            // 
+            // y_c
+            // 
+            this.y_c.Location = new System.Drawing.Point(1150, 109);
+            this.y_c.Name = "y_c";
+            this.y_c.Size = new System.Drawing.Size(100, 20);
+            this.y_c.TabIndex = 7;
+            // 
+            // h1
+            // 
+            this.h1.Location = new System.Drawing.Point(1150, 144);
+            this.h1.Name = "h1";
+            this.h1.Size = new System.Drawing.Size(100, 20);
+            this.h1.TabIndex = 8;
+            // 
+            // w1
+            // 
+            this.w1.Location = new System.Drawing.Point(1150, 174);
+            this.w1.Name = "w1";
+            this.w1.Size = new System.Drawing.Size(100, 20);
+            this.w1.TabIndex = 9;
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(1150, 234);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(100, 20);
+            this.y1.TabIndex = 10;
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(1150, 205);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(100, 20);
+            this.x1.TabIndex = 11;
+            // 
+            // chart1
+            // 
+            chartArea2.CursorX.IsUserSelectionEnabled = true;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(42, 49);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(932, 551);
+            this.chart1.TabIndex = 12;
+            this.chart1.Text = "graph";
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.BackColor = System.Drawing.Color.Green;
+            this.buttonDraw.Cursor = System.Windows.Forms.Cursors.No;
+            this.buttonDraw.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.buttonDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDraw.ForeColor = System.Drawing.Color.White;
+            this.buttonDraw.Location = new System.Drawing.Point(1150, 280);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(100, 23);
+            this.buttonDraw.TabIndex = 13;
+            this.buttonDraw.Text = "Построить";
+            this.buttonDraw.UseVisualStyleBackColor = false;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1150, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Cursor = System.Windows.Forms.Cursors.No;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(1150, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Построить А";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Green;
+            this.button3.Cursor = System.Windows.Forms.Cursors.No;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(1150, 389);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Построить B";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1397, 694);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.w1);
+            this.Controls.Add(this.h1);
+            this.Controls.Add(this.y_c);
+            this.Controls.Add(this.x_c);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox x_c;
+        private System.Windows.Forms.TextBox y_c;
+        private System.Windows.Forms.TextBox h1;
+        private System.Windows.Forms.TextBox w1;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ geomt/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace geomt
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Настройка графика
+            SetupChart();
+        }
+
+        private void SetupChart()
+        {
+           
+            Chart myChart = chart1 as Chart;
+            if (myChart == null) return;
+
+            myChart.Series.Clear();
+            double pp = 3;
+            // область построения
+            myChart.ChartAreas[0].AxisX.Title = "X";
+            myChart.ChartAreas[0].AxisY.Title = "Y";
+            myChart.ChartAreas[0].AxisX.IsStartedFromZero = false;
+            myChart.ChartAreas[0].AxisY.IsStartedFromZero = false;
+
+            //масштабирование
+            myChart.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            myChart.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            myChart.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+            myChart.ChartAreas[0].AxisY.ScrollBar.Enabled = true;
+            myChart.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            myChart.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+
+            Series rectSeries = new Series("Прямоугольник");
+            rectSeries.ChartType = SeriesChartType.Line;
+            rectSeries.Color = Color.Blue;
+            rectSeries.BorderWidth = 2;
+            myChart.Series.Add(rectSeries);
+
+            Series lineSeries = new Series("Отрезок");
+            lineSeries.ChartType = SeriesChartType.Line;
+            lineSeries.Color = Color.Red;
+            lineSeries.BorderWidth = 2;
+            myChart.Series.Add(lineSeries);
+
+            Series pointSeries = new Series("Точка пересечения");
+            pointSeries.ChartType = SeriesChartType.Point;
+            pointSeries.Color = Color.Green;
+            pointSeries.MarkerStyle = MarkerStyle.Circle;
+            pointSeries.MarkerSize = 8;
+            myChart.Series.Add(pointSeries);
+
+            Series centerSeries = new Series("Центр");
+            centerSeries.ChartType = SeriesChartType.Point;
+            centerSeries.Color = Color.Black;
+            centerSeries.MarkerStyle = MarkerStyle.Circle;
+            centerSeries.MarkerSize = 6;
+            myChart.Series.Add(centerSeries);
+
+
+            Series externalPointSeries = new Series("Внешняя точка");
+            externalPointSeries.ChartType = SeriesChartType.Point;
+            externalPointSeries.Color = Color.Orange;
+            externalPointSeries.MarkerStyle = MarkerStyle.Star4;  
+            externalPointSeries.MarkerSize = 12;
+            externalPointSeries.MarkerColor = Color.Orange;
+            myChart.Series.Add(externalPointSeries);
+        }
+
+        private void AutoScaleChart(Chart myChart, double x_c, double y_c, double h, double w, double x, double y)
+        {
+            double minX = Math.Min(Math.Min(x_c - w / 2, x_c + w / 2), x);
+            double maxX = Math.Max(Math.Max(x_c + w / 2, x_c - w / 2), x);
+            double minY = Math.Min(Math.Min(y_c - h / 2, y_c + h / 2), y);
+            double maxY = Math.Max(Math.Max(y_c + h / 2, y_c - h / 2), y);
+
+            // 20% от размера
+            double paddingX = (maxX - minX) * 0.2;
+            double paddingY = (maxY - minY) * 0.2;
+
+            if (paddingX == 0) paddingX = 1;
+            if (paddingY == 0) paddingY = 1;
+
+            myChart.ChartAreas[0].AxisX.Minimum = minX - paddingX;
+            myChart.ChartAreas[0].AxisX.Maximum = maxX + paddingX;
+            myChart.ChartAreas[0].AxisY.Minimum = minY - paddingY;
+            myChart.ChartAreas[0].AxisY.Maximum = maxY + paddingY;
+
+            myChart.ChartAreas[0].AxisX.Interval = 1;
+            myChart.ChartAreas[0].AxisY.Interval = 1;
+            myChart.ChartAreas[0].AxisX.MajorGrid.Interval = 1;
+            myChart.ChartAreas[0].AxisY.MajorGrid.Interval = 1;
+
+            myChart.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+            myChart.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+            myChart.ChartAreas[0].AxisX.MajorGrid.LineWidth = 1;
+
+            myChart.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            myChart.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+            myChart.ChartAreas[0].AxisY.MajorGrid.LineWidth = 1;
+        }
+
+        private void DrawRectangle(Chart myChart, double x_c, double y_c, double h, double w)
+        {
+            double left = x_c - w / 2;
+            double right = x_c + w / 2;
+            double top = y_c + h / 2;
+            double bottom = y_c - h / 2;
+
+            myChart.Series["Прямоугольник"].Points.AddXY(left, top);
+            myChart.Series["Прямоугольник"].Points.AddXY(right, top);
+            myChart.Series["Прямоугольник"].Points.AddXY(right, bottom);
+            myChart.Series["Прямоугольник"].Points.AddXY(left, bottom);
+            myChart.Series["Прямоугольник"].Points.AddXY(left, top);
+        }
+
+        private void DrawLine(Chart myChart, double x1, double y1, double x2, double y2)
+        {
+            myChart.Series["Отрезок"].Points.AddXY(x1, y1);
+            myChart.Series["Отрезок"].Points.AddXY(x2, y2);
+        }
+
+        private PointF? FindIntersection(double x_c, double y_c, double h, double w, double x, double y)
+        {
+            double left = x_c - w / 2;
+            double right = x_c + w / 2;
+            double bottom = y_c - h / 2;
+            double top = y_c + h / 2;
+
+            double dx = x - x_c;
+            double dy = y - y_c;
+
+            PointF? intersection = null;
+            double minT = double.MaxValue;
+
+            // Левая сторона
+            if (dx != 0)
+            {
+                double t = (left - x_c) / dx;
+                if (t > 0 && t < 1)
+                {
+                    double yIntersect = y_c + t * dy;
+                    if (yIntersect >= bottom && yIntersect <= top && t < minT)
+                    {
+                        minT = t;
+                        intersection = new PointF((float)left, (float)yIntersect);
+                    }
+                }
+            }
+
+            // Правая сторона
+            if (dx != 0)
+            {
+                double t = (right - x_c) / dx;
+                if (t > 0 && t < 1)
+                {
+                    double yIntersect = y_c + t * dy;
+                    if (yIntersect >= bottom && yIntersect <= top && t < minT)
+                    {
+                        minT = t;
+                        intersection = new PointF((float)right, (float)yIntersect);
+                    }
+                }
+            }
+
+            // Нижняя сторона
+            if (dy != 0)
+            {
+                double t = (bottom - y_c) / dy;
+                if (t > 0 && t < 1)
+                {
+                    double xIntersect = x_c + t * dx;
+                    if (xIntersect >= left && xIntersect <= right && t < minT)
+                    {
+                        minT = t;
+                        intersection = new PointF((float)xIntersect, (float)bottom);
+                    }
+                }
+            }
+
+            // Верхняя сторона
+            if (dy != 0)
+            {
+                double t = (top - y_c) / dy;
+                if (t > 0 && t < 1)
+                {
+                    double xIntersect = x_c + t * dx;
+                    if (xIntersect >= left && xIntersect <= right && t < minT)
+                    {
+                        minT = t;
+                        intersection = new PointF((float)xIntersect, (float)top);
+                    }
+                }
+            }
+
+            return intersection;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Chart myChart = chart1 as Chart;
+            foreach (var series in myChart.Series)
+            {
+                series.Points.Clear();
+            }
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e) { DrawWithMode(0); }
+        private void button2_Click(object sender, EventArgs e) { DrawWithMode(1); }
+        private void button3_Click(object sender, EventArgs e) { DrawWithMode(2); }
+
+        private void DrawWithMode(int mode)
+        {
+            try
+            {
+                double xc = double.Parse(x_c.Text);
+                double yc = double.Parse(y_c.Text);
+                double h = double.Parse(h1.Text);
+                double w = double.Parse(w1.Text);
+                double x = double.Parse(x1.Text);
+                double y = double.Parse(y1.Text);
+
+                Chart myChart = chart1 as Chart;
+                if (myChart == null) return;
+
+                foreach (var series in myChart.Series) series.Points.Clear();
+
+                DrawRectangle(myChart, xc, yc, h, w);
+
+                myChart.Series["Центр"].Points.AddXY(xc, yc);
+                myChart.Series["Центр"].Points[0].Label = $"({xc:F1}; {yc:F1})";
+
+                myChart.Series["Внешняя точка"].Points.AddXY(x, y);
+                myChart.Series["Внешняя точка"].Points[0].Label = $"({x:F1}; {y:F1})";
+
+                PointF? intersectionPoint = FindIntersection(xc, yc, h, w, x, y);
+
+                if (intersectionPoint.HasValue)
+                {
+                    myChart.Series["Точка пересечения"].Points.AddXY(intersectionPoint.Value.X, intersectionPoint.Value.Y);
+                    myChart.Series["Точка пересечения"].Points[0].Label = $"({intersectionPoint.Value.X:F1}; {intersectionPoint.Value.Y:F1})";
+
+                    if (mode == 0) DrawLine(myChart, xc, yc, x, y);
+                    if (mode == 1) DrawLine(myChart, xc, yc, intersectionPoint.Value.X, intersectionPoint.Value.Y);
+                    if (mode == 2) DrawLine(myChart, intersectionPoint.Value.X, intersectionPoint.Value.Y, x, y);
+
+                    MessageBox.Show($"Точка пересечения: ({intersectionPoint.Value.X:F1}, {intersectionPoint.Value.Y:F1})");
+                }
+                else
+                {
+                    if (h == x && w == y && xc != x && yc != y)
+                    {
+                        myChart.Series["Точка пересечения"].Points.AddXY(x, y);
+                        myChart.Series["Точка пересечения"].Points[0].Label = $"({x:F1}; {y:F1})";
+
+                        if (mode == 0) DrawLine(myChart, xc, yc, x, y);
+                        if (mode == 1) DrawLine(myChart, xc, yc, x, y);
+
+                        MessageBox.Show("Выбранная внешняя точка совпадает с границей прямоугольника!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Отрезок не пересекает прямоугольник!");
+                        foreach (var series in myChart.Series) series.Points.Clear();
+                    }
+                }
+
+                AutoScaleChart(myChart, xc, yc, h, w, x, y);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числа!", "Ошибка ввода");
+            }
+        }
+    }
+}
