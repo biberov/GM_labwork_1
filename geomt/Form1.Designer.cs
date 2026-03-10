a0d3234627f79c5ff7774937018e656917380dd7
Andrey Saberov <saberoff2018@yandex.ru>
2026-03-10 23:29:54 +0300

    Пояснения к функциям Form1.cs

--- geomt/Form1.Designer.cs
+++ geomt/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -45,9 +45,7 @@
             this.x1 = new System.Windows.Forms.TextBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.buttonDraw = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -149,17 +147,17 @@
             // 
             // chart1
             // 
-            chartArea2.CursorX.IsUserSelectionEnabled = true;
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.CursorX.IsUserSelectionEnabled = true;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(42, 49);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(932, 551);
             this.chart1.TabIndex = 12;
             this.chart1.Text = "graph";
@@ -167,7 +165,7 @@
             // buttonDraw
             // 
             this.buttonDraw.BackColor = System.Drawing.Color.Green;
-            this.buttonDraw.Cursor = System.Windows.Forms.Cursors.No;
+            this.buttonDraw.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonDraw.FlatAppearance.BorderColor = System.Drawing.Color.Green;
             this.buttonDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonDraw.ForeColor = System.Drawing.Color.White;
@@ -179,54 +177,22 @@
             this.buttonDraw.UseVisualStyleBackColor = false;
             this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
             // 
-            // button1
+            // ClearButton
             // 
-            this.button1.Location = new System.Drawing.Point(1150, 309);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "Очистить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Green;
-            this.button2.Cursor = System.Windows.Forms.Cursors.No;
-            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Green;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(1150, 360);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 23);
-            this.button2.TabIndex = 15;
-            this.button2.Text = "Построить А";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Green;
-            this.button3.Cursor = System.Windows.Forms.Cursors.No;
-            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Green;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(1150, 389);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 23);
-            this.button3.TabIndex = 16;
-            this.button3.Text = "Построить B";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.ClearButton.Location = new System.Drawing.Point(1150, 309);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(100, 23);
+            this.ClearButton.TabIndex = 14;
+            this.ClearButton.Text = "Очистить";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1397, 694);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ClearButton);
             this.Controls.Add(this.buttonDraw);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.x1);
@@ -265,9 +231,7 @@
         private System.Windows.Forms.TextBox x1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button buttonDraw;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button ClearButton;
     }
 }
 
--- geomt/Form1.cs
+++ geomt/Form1.cs
@@ -12,26 +12,27 @@ namespace geomt
         public Form1()
         {
             InitializeComponent();
-
-            // Настройка графика
-            SetupChart();
+            SetupChart(); // Настройка графика при создании формы
         }
 
+        /// <summary>
+        /// Настраивает внешний вид и свойства графика (Chart).
+        /// Создает серии для отображения прямоугольника, отрезка, точек пересечения, центра и внешней точки.
+        /// </summary>
         private void SetupChart()
         {
-           
             Chart myChart = chart1 as Chart;
             if (myChart == null) return;
 
             myChart.Series.Clear();
-            double pp = 3;
-            // область построения
+
+            // Настройка осей координат
             myChart.ChartAreas[0].AxisX.Title = "X";
             myChart.ChartAreas[0].AxisY.Title = "Y";
             myChart.ChartAreas[0].AxisX.IsStartedFromZero = false;
             myChart.ChartAreas[0].AxisY.IsStartedFromZero = false;
 
-            //масштабирование
+            // Включение масштабирования и прокрутки
             myChart.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
             myChart.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
             myChart.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
@@ -39,25 +40,29 @@ namespace geomt
             myChart.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
             myChart.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
 
+            // Серия для отображения прямоугольника
             Series rectSeries = new Series("Прямоугольник");
             rectSeries.ChartType = SeriesChartType.Line;
             rectSeries.Color = Color.Blue;
             rectSeries.BorderWidth = 2;
             myChart.Series.Add(rectSeries);
 
+            // Серия для отображения отрезка
             Series lineSeries = new Series("Отрезок");
             lineSeries.ChartType = SeriesChartType.Line;
             lineSeries.Color = Color.Red;
             lineSeries.BorderWidth = 2;
             myChart.Series.Add(lineSeries);
 
+            // Серия для отображения точки пересечения
             Series pointSeries = new Series("Точка пересечения");
             pointSeries.ChartType = SeriesChartType.Point;
             pointSeries.Color = Color.Green;
             pointSeries.MarkerStyle = MarkerStyle.Circle;
             pointSeries.MarkerSize = 8;
             myChart.Series.Add(pointSeries);
-            double m = 1;
+
+            // Серия для отображения центра прямоугольника
             Series centerSeries = new Series("Центр");
             centerSeries.ChartType = SeriesChartType.Point;
             centerSeries.Color = Color.Black;
@@ -65,46 +70,54 @@ namespace geomt
             centerSeries.MarkerSize = 6;
             myChart.Series.Add(centerSeries);
 
-
+            // Серия для отображения внешней точки (конца отрезка)
             Series externalPointSeries = new Series("Внешняя точка");
             externalPointSeries.ChartType = SeriesChartType.Point;
             externalPointSeries.Color = Color.Orange;
-            externalPointSeries.MarkerStyle = MarkerStyle.Star4;  
+            externalPointSeries.MarkerStyle = MarkerStyle.Star4;
             externalPointSeries.MarkerSize = 12;
             externalPointSeries.MarkerColor = Color.Orange;
             myChart.Series.Add(externalPointSeries);
-<<<<<<< HEAD
-            double c = 1;
-            c+=1;
-=======
->>>>>>> e90d64e (всё четко! исправление 1 строкиInitial commit)
         }
 
+        /// <summary>
+        /// Автоматически масштабирует график, чтобы все элементы были видны с отступами.
+        /// </summary>
+        /// <param name="myChart">Объект графика для масштабирования</param>
+        /// <param name="x_c">X-координата центра прямоугольника</param>
+        /// <param name="y_c">Y-координата центра прямоугольника</param>
+        /// <param name="h">Высота прямоугольника</param>
+        /// <param name="w">Ширина прямоугольника</param>
+        /// <param name="x">X-координата внешней точки</param>
+        /// <param name="y">Y-координата внешней точки</param>
         private void AutoScaleChart(Chart myChart, double x_c, double y_c, double h, double w, double x, double y)
         {
+            // Вычисление минимальных и максимальных значений координат
             double minX = Math.Min(Math.Min(x_c - w / 2, x_c + w / 2), x);
             double maxX = Math.Max(Math.Max(x_c + w / 2, x_c - w / 2), x);
             double minY = Math.Min(Math.Min(y_c - h / 2, y_c + h / 2), y);
             double maxY = Math.Max(Math.Max(y_c + h / 2, y_c - h / 2), y);
-            double paf = 3;
-            // 20% от размера
+
+            // Добавление отступов (20% от размера)
             double paddingX = (maxX - minX) * 0.2;
             double paddingY = (maxY - minY) * 0.2;
 
             if (paddingX == 0) paddingX = 1;
             if (paddingY == 0) paddingY = 1;
-            double m = 4;
-            double pas = 5;
+
+            // Установка границ осей
             myChart.ChartAreas[0].AxisX.Minimum = minX - paddingX;
             myChart.ChartAreas[0].AxisX.Maximum = maxX + paddingX;
             myChart.ChartAreas[0].AxisY.Minimum = minY - paddingY;
             myChart.ChartAreas[0].AxisY.Maximum = maxY + paddingY;
 
+            // Настройка сетки
             myChart.ChartAreas[0].AxisX.Interval = 1;
             myChart.ChartAreas[0].AxisY.Interval = 1;
             myChart.ChartAreas[0].AxisX.MajorGrid.Interval = 1;
             myChart.ChartAreas[0].AxisY.MajorGrid.Interval = 1;
 
+            // Настройка внешнего вида сетки
             myChart.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
             myChart.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
             myChart.ChartAreas[0].AxisX.MajorGrid.LineWidth = 1;
@@ -114,13 +127,23 @@ namespace geomt
             myChart.ChartAreas[0].AxisY.MajorGrid.LineWidth = 1;
         }
 
+        /// <summary>
+        /// Рисует прямоугольник на графике по заданным параметрам.
+        /// </summary>
+        /// <param name="myChart">Объект графика</param>
+        /// <param name="x_c">X-координата центра</param>
+        /// <param name="y_c">Y-координата центра</param>
+        /// <param name="h">Высота прямоугольника</param>
+        /// <param name="w">Ширина прямоугольника</param>
         private void DrawRectangle(Chart myChart, double x_c, double y_c, double h, double w)
         {
+            // Вычисление координат углов прямоугольника
             double left = x_c - w / 2;
             double right = x_c + w / 2;
             double top = y_c + h / 2;
             double bottom = y_c - h / 2;
 
+            // Добавление точек для построения замкнутого прямоугольника
             myChart.Series["Прямоугольник"].Points.AddXY(left, top);
             myChart.Series["Прямоугольник"].Points.AddXY(right, top);
             myChart.Series["Прямоугольник"].Points.AddXY(right, bottom);
@@ -128,26 +151,147 @@ namespace geomt
             myChart.Series["Прямоугольник"].Points.AddXY(left, top);
         }
 
+        /// <summary>
+        /// Рисует отрезок на графике от центра до внешней точки.
+        /// </summary>
+        /// <param name="myChart">Объект графика</param>
+        /// <param name="x1">X-координата начала отрезка (центр)</param>
+        /// <param name="y1">Y-координата начала отрезка (центр)</param>
+        /// <param name="x2">X-координата конца отрезка (внешняя точка)</param>
+        /// <param name="y2">Y-координата конца отрезка (внешняя точка)</param>
         private void DrawLine(Chart myChart, double x1, double y1, double x2, double y2)
         {
             myChart.Series["Отрезок"].Points.AddXY(x1, y1);
             myChart.Series["Отрезок"].Points.AddXY(x2, y2);
         }
 
+        /// <summary>
+        /// Обработчик нажатия кнопки "Нарисовать".
+        /// Считывает входные данные, строит прямоугольник, отрезок и ищет точку пересечения.
+        /// </summary>
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Считывание данных из текстовых полей
+                double xc = double.Parse(x_c.Text);
+                double yc = double.Parse(y_c.Text);
+                double h = double.Parse(h1.Text);
+                double w = double.Parse(w1.Text);
+                double x = double.Parse(x1.Text);
+                double y = double.Parse(y1.Text);
+
+                Chart myChart = chart1 as Chart;
+                if (myChart == null) return;
+
+                // Очистка всех серий
+                foreach (var series in myChart.Series)
+                {
+                    series.Points.Clear();
+                }
+
+                // Построение прямоугольника
+                DrawRectangle(myChart, xc, yc, h, w);
+
+                // Построение отрезка
+                DrawLine(myChart, xc, yc, x, y);
+
+                // Добавление внешней точки
+                myChart.Series["Внешняя точка"].Points.AddXY(x, y);
+
+                // Добавление подписи к внешней точке
+                var externalPoint = myChart.Series["Внешняя точка"].Points[0];
+                externalPoint.Label = $"({x:F1}; {y:F1})";
+                externalPoint.LabelForeColor = Color.Orange;
+                externalPoint.Font = new Font("Arial", 9, FontStyle.Bold);
+
+                // Добавление центра прямоугольника
+                myChart.Series["Центр"].Points.AddXY(xc, yc);
+                var center = myChart.Series["Центр"].Points[0];
+                center.Label = $"({xc:F1}; {yc:F1})";
+                center.LabelForeColor = Color.Black;
+                center.Font = new Font("Arial", 9, FontStyle.Bold);
+
+                // Поиск точки пересечения
+                PointF? intersectionPoint = FindIntersection(xc, yc, h, w, x, y);
+
+                if (intersectionPoint.HasValue)
+                {
+                    // Добавление найденной точки пересечения
+                    myChart.Series["Точка пересечения"].Points.AddXY(intersectionPoint.Value.X, intersectionPoint.Value.Y);
+
+                    var interPoint = myChart.Series["Точка пересечения"].Points[0];
+                    interPoint.Label = $"({intersectionPoint.Value.X:F1}; {intersectionPoint.Value.Y:F1})";
+                    interPoint.LabelForeColor = Color.Green;
+                    interPoint.Font = new Font("Arial", 9, FontStyle.Bold);
+
+                    MessageBox.Show($"Точка пересечения: ({intersectionPoint.Value.X:F1}, {intersectionPoint.Value.Y:F1})",
+                        "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // Проверка особого случая - точка на границе
+                    if (h == x && w == y && xc != x && yc != y)
+                    {
+                        myChart.Series["Точка пересечения"].Points.AddXY(x, y);
+
+                        var interPoint = myChart.Series["Точка пересечения"].Points[0];
+                        interPoint.Label = $"({x:F1}; {y:F1})";
+                        interPoint.LabelForeColor = Color.Green;
+                        interPoint.Font = new Font("Arial", 9, FontStyle.Bold);
+                        MessageBox.Show("Выбранная внешняя точка совпадает с границей прямоугольника!", "Информация",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Отрезок не пересекает прямоугольник!", "Информация",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // Очистка всех серий при отсутствии пересечения
+                        foreach (var series in myChart.Series)
+                        {
+                            series.Points.Clear();
+                        }
+                    }
+                }
+
+                // Автоматическое масштабирование графика
+                AutoScaleChart(myChart, xc, yc, h, w, x, y);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числа!", "Ошибка ввода",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Находит точку пересечения отрезка от центра до внешней точки с прямоугольником.
+        /// Использует параметрическое уравнение прямой и проверяет пересечения со всеми сторонами.
+        /// </summary>
+        /// <param name="x_c">X-координата центра прямоугольника</param>
+        /// <param name="y_c">Y-координата центра прямоугольника</param>
+        /// <param name="h">Высота прямоугольника</param>
+        /// <param name="w">Ширина прямоугольника</param>
+        /// <param name="x">X-координата внешней точки</param>
+        /// <param name="y">Y-координата внешней точки</param>
+        /// <returns>Точка пересечения или null, если пересечения нет</returns>
         private PointF? FindIntersection(double x_c, double y_c, double h, double w, double x, double y)
         {
+            // Вычисление границ прямоугольника
             double left = x_c - w / 2;
             double right = x_c + w / 2;
             double bottom = y_c - h / 2;
             double top = y_c + h / 2;
 
+            // Вектор направления от центра к внешней точке
             double dx = x - x_c;
             double dy = y - y_c;
 
             PointF? intersection = null;
-            double minT = double.MaxValue;
+            double minT = double.MaxValue; // Ближайшее пересечение (наименьший параметр t)
 
-            // Левая сторона
+            // Проверка пересечения с левой стороной
             if (dx != 0)
             {
                 double t = (left - x_c) / dx;
@@ -162,7 +306,7 @@ namespace geomt
                 }
             }
 
-            // Правая сторона
+            // Проверка пересечения с правой стороной
             if (dx != 0)
             {
                 double t = (right - x_c) / dx;
@@ -177,7 +321,7 @@ namespace geomt
                 }
             }
 
-            // Нижняя сторона
+            // Проверка пересечения с нижней стороной
             if (dy != 0)
             {
                 double t = (bottom - y_c) / dy;
@@ -192,7 +336,7 @@ namespace geomt
                 }
             }
 
-            // Верхняя сторона
+            // Проверка пересечения с верхней стороной
             if (dy != 0)
             {
                 double t = (top - y_c) / dy;
@@ -210,81 +354,16 @@ namespace geomt
             return intersection;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Обработчик кнопки "Очистить".
+        /// Удаляет все точки со всех серий графика.
+        /// </summary>
+        private void ClearButton_Click(object sender, EventArgs e)
         {
-
             Chart myChart = chart1 as Chart;
             foreach (var series in myChart.Series)
             {
                 series.Points.Clear();
-            }
-        }
-
-        private void buttonDraw_Click(object sender, EventArgs e) { DrawWithMode(0); }
-        private void button2_Click(object sender, EventArgs e) { DrawWithMode(1); }
-        private void button3_Click(object sender, EventArgs e) { DrawWithMode(2); }
-
-        private void DrawWithMode(int mode)
-        {
-            try
-            {
-                double xc = double.Parse(x_c.Text);
-                double yc = double.Parse(y_c.Text);
-                double h = double.Parse(h1.Text);
-                double w = double.Parse(w1.Text);
-                double x = double.Parse(x1.Text);
-                double y = double.Parse(y1.Text);
-
-                Chart myChart = chart1 as Chart;
-                if (myChart == null) return;
-
-                foreach (var series in myChart.Series) series.Points.Clear();
-
-                DrawRectangle(myChart, xc, yc, h, w);
-
-                myChart.Series["Центр"].Points.AddXY(xc, yc);
-                myChart.Series["Центр"].Points[0].Label = $"({xc:F1}; {yc:F1})";
-
-                myChart.Series["Внешняя точка"].Points.AddXY(x, y);
-                myChart.Series["Внешняя точка"].Points[0].Label = $"({x:F1}; {y:F1})";
-
-                PointF? intersectionPoint = FindIntersection(xc, yc, h, w, x, y);
-
-                if (intersectionPoint.HasValue)
-                {
-                    myChart.Series["Точка пересечения"].Points.AddXY(intersectionPoint.Value.X, intersectionPoint.Value.Y);
-                    myChart.Series["Точка пересечения"].Points[0].Label = $"({intersectionPoint.Value.X:F1}; {intersectionPoint.Value.Y:F1})";
-
-                    if (mode == 0) DrawLine(myChart, xc, yc, x, y);
-                    if (mode == 1) DrawLine(myChart, xc, yc, intersectionPoint.Value.X, intersectionPoint.Value.Y);
-                    if (mode == 2) DrawLine(myChart, intersectionPoint.Value.X, intersectionPoint.Value.Y, x, y);
-
-                    MessageBox.Show($"Точка пересечения: ({intersectionPoint.Value.X:F1}, {intersectionPoint.Value.Y:F1})");
-                }
-                else
-                {
-                    if (h == x && w == y && xc != x && yc != y)
-                    {
-                        myChart.Series["Точка пересечения"].Points.AddXY(x, y);
-                        myChart.Series["Точка пересечения"].Points[0].Label = $"({x:F1}; {y:F1})";
-
-                        if (mode == 0) DrawLine(myChart, xc, yc, x, y);
-                        if (mode == 1) DrawLine(myChart, xc, yc, x, y);
-
-                        MessageBox.Show("Выбранная внешняя точка совпадает с границей прямоугольника!");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Отрезок не пересекает прямоугольник!");
-                        foreach (var series in myChart.Series) series.Points.Clear();
-                    }
-                }
-
-                AutoScaleChart(myChart, xc, yc, h, w, x, y);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Пожалуйста, введите корректные числа!", "Ошибка ввода");
             }
         }
     }
